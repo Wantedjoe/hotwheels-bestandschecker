7d2493052bbc9ed6ad37ed89c337df1eabfa49f3
Wantedjoe <andre.wipf@gmx.ch>
2026-09-14 23:12:07 +0200

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,8 +1,10 @@
 import os
 import requests
 
-URL = "https://diecasthunter.de/products/hot-wheels-2026-team-transport-porsche-rexy-911-gt3-r-992-fleet-flyer-1-64"
+# URL = "https://diecasthunter.de/products/hot-wheels-2026-team-transport-porsche-rexy-911-gt3-r-992-fleet-flyer-1-64"
 # URL = "https://diecasthunter.de/products/hot-wheels-boulevard-155-2021-toyota-gr-supra-1-64"
+URL = "https://diecasthunter.de/products/hot-wheels-2026-pop-culture-forza-audi-90-quattro"
+
 
 BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
