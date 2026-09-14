93d23987d73662d012b47ce9b24629c6c5bf41e3
Wantedjoe <andre.wipf@gmx.ch>
2026-09-14 12:49:41 +0200

    Fix out_of_stock condition check in checker.py

--- checker.py
+++ checker.py
@@ -20,9 +20,8 @@ page = response.text.lower()
 
 # Solange diese Begriffe auf der Seite stehen,
 # gehen wir davon aus, dass der Artikel noch nicht verfügbar ist.
-out_of_stock = (
-    "nicht vorrätig" in page
-    or "ausverkauft" in page
+out_of_stock != (
+    "In den Warenkorb legen" in page
 )
 
 if not out_of_stock:
