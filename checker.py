da099b97aee96a0101772be65956265cd97cae88
Wantedjoe <andre.wipf@gmx.ch>
2026-09-14 08:27:30 +0200

    Swap URLs for Hot Wheels product in checker.py

--- checker.py
+++ checker.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-# URL = "https://diecasthunter.de/products/hot-wheels-factory-set-2026-1-4-inkl-hot-wheels-datsun-510-gasser-chase-1-64"
-URL = https://diecasthunter.de/products/hot-wheels-boulevard-155-2021-toyota-gr-supra-1-64
+URL = "https://diecasthunter.de/products/hot-wheels-factory-set-2026-1-4-inkl-hot-wheels-datsun-510-gasser-chase-1-64"
+# URL = https://diecasthunter.de/products/hot-wheels-boulevard-155-2021-toyota-gr-supra-1-64
 
 BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
